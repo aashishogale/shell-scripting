c4630385b9a52f83de9605f56331a7905c17b5ee
aashishogale <aashishogale9@gmail.com>
2019-12-30 15:25:04 +0530

    covert to dictionary

--- arithmeticComputation.sh
+++ arithmeticComputation.sh
@@ -9,3 +9,22 @@ operation1=$(($a+$b*$c))
 operation2=$(($a*$b+$c))
 echo "first operation" $operation1
 echo "second operation" $operation
+operation1=$(($a+$b*$c))
+operation2=$(($a*$b+$c))
+operation3=$(($c+$a/$b))
+operation4=$(($a%$b+$c))
+echo "first operation" $operation1
+echo "second operation" $operation2
+echo "third operation" $operation3
+echo "fourth operation" $operation4
+
+resultDictionary[0]=$operation1
+resultDictionary[1]=$operation2
+resultDictionary[2]=$operation3
+resultDictionary[3]=$operation4
+
+for i in ${!resultDictionary[@]}
+do
+	resultArray[$i]=${resultDictionary[$i]}
+done
+
