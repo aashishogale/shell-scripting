55f0358fa010e61de4b8217f9273c4d5ff602d6c
aashishogale <aashishogale9@gmail.com>
2019-12-30 16:20:34 +0530

    calculating amount for 20 days

--- gamblingsim.sh
+++ gamblingsim.sh
@@ -26,17 +26,48 @@ function dailyPlay()
 }
 dailyPlay
 
+#!/bin/bash -x
+
 echo "WELCOME TO GAMBLING SIMULATION"
 
 STAKE_PER_DAY=100
 BET=1
+MAX_STAKE=$(($STAKE_PER_DAY+(50*$STAKE_PER_DAY/100)))
+MIN_STAKE=$(($STAKE_PER_DAY-(50*$STAKE_PER_DAY/100)))
+DAYS=20
 
 #variables
-winOrLoss=$((RANDOM%2))
+cash=$STAKE_PER_DAY
+gainAmount=0
+totalProfit=0
+function dailyPlay()
+{
+	while [ $cash -gt $MIN_STAKE ] && [ $cash -lt $MAX_STAKE ]
+	do
+		random=$((RANDOM%2))
+		if [ $random == 1 ]
+		then
+			cash=$(($cash+$BET))
+		else
+			cash=$(($cash-$BET))
+		fi
+	done
 
-if [ winOrLoss -eq 1]
-then
-	echo "win"
-else
-	echo "loss"
-fi
+	gainAmount=$(($cash-100))
+	echo $gainAmount
+}
+
+function profitOfParticularDays()
+{
+        local day=1
+        while [ $day -lt $DAYS ]
+        do
+                local profitOfTheDay=0
+                profitOfTheDay=$(dailyPlay)
+                dayProfit[$day]=$gainAmount
+                day=$(($day+1))
+                totalProfit=$(($totalProfit+$profitOfTheDay))
+        done
+        echo "total profit :"$totalProfit
+}
+profitOfParticularDays
