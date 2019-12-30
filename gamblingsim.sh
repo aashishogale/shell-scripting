5e7507896fbcb30fbcf0a5eb04545ee868756bc3
aashishogale <aashishogale9@gmail.com>
2019-12-30 16:03:06 +0530

    added daily play

--- gamblingsim.sh
+++ gamblingsim.sh
@@ -1,5 +1,31 @@
 #!/bin/bash -x
 
+STAKE_PER_DAY=100
+BET=1
+MAX_STAKE=$(($STAKE_PER_DAY+(50*$STAKE_PER_DAY/100)))
+MIN_STAKE=$(($STAKE_PER_DAY-(50*$STAKE_PER_DAY/100)))
+
+#variables
+cash=$STAKE_PER_DAY
+
+function dailyPlay()
+{
+	while [ $cash -gt $MIN_STAKE ] && [ $cash -lt $MAX_STAKE ]
+	do
+		random=$((RANDOM%2))
+		if [ $random == 1 ]
+		then
+			cash=$(($cash+$BET))
+			echo $cash
+		else
+			cash=$(($cash-$BET))
+			echo $cash
+		fi
+	done
+	echo $cash
+}
+dailyPlay
+
 echo "WELCOME TO GAMBLING SIMULATION"
 
 STAKE_PER_DAY=100
